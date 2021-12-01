2065737bc3120018032c1b0e350ef2b3a836edf4
Longtime Developer <79998776+longtimedeveloper@users.noreply.github.com>
2021-11-30 23:47:36 -0500

    fix commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "97c36c7fb771d24a1a83c03c7331f5f4",
-"/": "97c36c7fb771d24a1a83c03c7331f5f4",
+"index.html": "4ca9562bd16f6e2df541ebb0c8e38a74",
+"/": "4ca9562bd16f6e2df541ebb0c8e38a74",
 "main.dart.js": "211f0fa85be385e6a6418c85d9dd551f",
 "manifest.json": "b4f0fce2a9692c354c85822af84888ab",
 "version.json": "eccd9ed59c76a2ae4c189a820f2490c6"
